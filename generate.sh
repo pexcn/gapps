82f71d588166783b2d7209c761b1bfdcdce69936
pexcn <i@pexcn.me>
2019-08-27 15:42:58 +0800

    update dist filename

--- generate.sh
+++ generate.sh
@@ -24,12 +24,12 @@ function packing_zip() {
 
 # API-25, ARMv7a, 480DPI
 merge_files "src/commons/*" "src/apps/commons/*" "src/apps/api-25/commons/*" "src/apps/api-25/arm-v7a/480dpi/*" "src/frameworks/api-25/*" "src/etcs/api-25/*"
-packing_zip "gapps_api25_armv7a_480dpi.zip"
+packing_zip "gapps-api25-armv7a-480dpi.zip"
 
 # API-27, ARMv7a, 480DPI
 merge_files "src/commons/*" "src/apps/commons/*" "src/apps/api-27/commons/*" "src/apps/api-27/arm-v7a/480dpi/*" "src/frameworks/api-27/*" "src/etcs/api-27/*"
-packing_zip "gapps_api27_armv7a_480dpi.zip"
+packing_zip "gapps-api27-armv7a-480dpi.zip"
 
 # API-28, ARMv7a, 480DPI
 merge_files "src/commons/*" "src/apps/commons/*" "src/apps/api-28/commons/*" "src/apps/api-28/arm-v7a/480dpi/*" "src/frameworks/api-28/*" "src/etcs/api-28/*"
-packing_zip "gapps_api28_armv7a_480dpi.zip"
+packing_zip "gapps-api28-armv7a-480dpi.zip"
